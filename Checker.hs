fd0588add8e450d40253a832ea3c551011320d10
Mathias Ramilo <mathiramilo2290@gmail.com>
2023-06-09 02:42:57 -0300

    [FASE DE CHEQUEO] Chequeo de nombres no declarados implementado

--- Checker.hs
+++ Checker.hs
@@ -40,12 +40,17 @@ instance Show Error where
    = "Expected: " ++ show ty ++ " Actual: " ++ show ty'
 
 
+
+-- Primer chequeo, repeticion de nombres. Si no hay errores, se devuelve 'Ok'. Si hay errores, se devuelven los errores.
+checkRepeatedNames :: Program -> Checked
+checkRepeatedNames (Program defs _) = if (checkFunctionDeclarations (Program defs _) == []) && (checkParamsNames (Program defs _) == []) then Ok else Wrong (checkFunctionDeclarations (Program defs _) ++ checkParamsNames (Program defs _))
+
 -- Chequeo de multiples declaraciones de una misma funcion. Dado un programa, esta funcion devuelve una lista de errores tal que cada vez que se repite la declaracion de una funcion se agrega un error 'Duplicated' a la lista.
 checkFunctionDeclarations :: Program -> [Error]
 checkFunctionDeclarations (Program defs _) = findRepeated (map getFunctionName defs)
 
 -- Dada una definicion de funcion, devuelve el nombre de la funcion.
-getFunctionName :: Def -> String
+getFunctionName :: FunDef -> String
 getFunctionName (FunDef (name, _) _ _) = name
 
 -- Dada una lista de strings, devuelve una lista de errores de forma que cada vez que un string se repite se agrega el correspondiente error a la lista.
@@ -63,47 +68,76 @@ checkParamsNames :: Program -> [Error]
 checkParamsNames (Program defs _) = findRepeatedParams (map getFunctionParams defs)
 
 -- Dada una definicion de funcion, devuelve los parametros de la funcion.
-getFunctionParams :: Def -> [String]
+getFunctionParams :: FunDef -> [String]
 getFunctionParams (FunDef (_, _) params _) = params
 
 -- Dada una lista de listas de strings, devuelve una lista de errores de forma que cada vez que un string se repite dentro de una sublista se agrega el correspondiente error a la lista.
 findRepeatedParams :: [[String]] -> [Error]
 findRepeatedParams xs = concatMap findRepeated xs
 
--- Primer chequeo, repeticion de nombres. Si no hay errores, se devuelve 'Ok'. Si hay errores, se devuelven los errores.
-checkRepeatedNames :: Program -> Checked
-checkRepeatedNames (Program defs _) = if (checkFunctionDeclarations (Program defs _) == []) && (checkParamsNames (Program defs _) == []) then Ok else Wrong (checkFunctionDeclarations (Program defs _) ++ checkParamsNames (Program defs _))
+
+-- Tercer chequeo, nombres no declarados. Si no hay errores, se devuelve 'Ok'. Si hay errores, se devuelven los errores.
+checkUndefinedNames :: Program -> Checked
+checkUndefinedNames (Program defs expr) = if (checkUndefinedNamesFuncDecl (Program defs expr) == []) && (checkUndefinedNamesMain (Program defs expr) == []) then Ok else Wrong (checkUndefinedNamesFuncDecl (Program defs expr) ++ checkUndefinedNamesMain (Program defs expr))
+
+-- Chequeo de nombres no declarados en la declaracion de funciones.
+checkUndefinedNamesFuncDecl :: Program -> [Error]
+checkUndefinedNamesFuncDecl (Program defs _) = concatMap (checkUndefinedNamesFuncDecl' defs)
+
+checkUndefinedNamesFuncDecl' :: FunDef -> [Error]
+checkUndefinedNamesFuncDecl' (FunDef (name, _) params expr) = checkUndefindedNamesExpr expr params
+
+-- Chequeo de nombres no declarados en la expresion principal.
+checkUndefinedNamesMain :: Program -> [Error]
+checkUndefinedNamesMain (Program defs expr) = checkUndefindedNamesExpr expr (map getFunctionName defs)
+
+-- Dada una expresion y una lista de nombres declarados, esta funcion devuelve una lista de errores tal que cada vez que se encuentra un nombre no declarado se agrega un error 'Undefined' a la lista.
+checkUndefinedNamesExpr :: Expr -> [String] -> [Error]
+checkUndefinedNamesExpr expr declaredNames = go expr declaredNames []
+  where 
+    go :: Expr -> [String] -> [Error] -> [Error]
+    go (Var name) declaredNames errors
+      | name `elem` declaredNames = errors
+      | otherwise = errors ++ [Undefined name]
+    go (IntLit _) _ errors = errors
+    go (BoolLit _) _ errors = errors
+    go (Infix _ expr1 expr2) declaredNames errors = go expr1 declaredNames (go expr2 declaredNames errors)
+    go (If expr1 expr2 expr3) declaredNames errors = go expr1 declaredNames (go expr2 declaredNames (go expr3 declaredNames errors))
+    go (Let (name, _) expr1 expr2) declaredNames errors = go expr1 declaredNames (go expr2 (declaredNames ++ [name]) errors)
+    go (App _ exprs) declaredNames errors = concatMap (\expr -> go expr declaredNames errors) exprs
 
 
--- funcion(Var a) = table
--- funcion(_ a) = tal
+-- data Type = TyInt | TyBool
+--  deriving Eq
 
--- sum1 = sumacc 0
---   where sumacc s []      = s 
---         sumacc s (x:xs)  = sumacc (s + x) xs
+-- data Program = Program Defs Expr
 
--- fact n = factacc 1 n
--- where
--- factacc a 0 = a
--- factacc a n = factacc (a ∗ n) (n − 1)
+-- type Name  = String
+
+-- type Defs = [FunDef]
+
+-- type TypedVar = (Name, Type)
+-- type TypedFun = (Name, Sig)
+
+-- data FunDef  = FunDef TypedFun [Name]  Expr
+
+-- data Sig = Sig [Type] Type
+
+-- data Expr = Var     Name
+--           | IntLit  Integer
+--           | BoolLit Bool
+--           | Infix   Op Expr Expr
+--           | If      Expr Expr Expr
+--           | Let     TypedVar Expr Expr
+--           | App     Name [Expr]
+
+-- data Op = Add | Sub | Mult | Div
+--         | Eq | NEq | GTh | LTh | GEq | LEq
+--         deriving Eq
+
+-- type Env = [TypedVar]
 
 
-checkVariableNames Program (Defs defs) (Expr [FunDef]) = -- foldl (\) []
-  | (condicion) = resultado
-  | (condicion) = resultado
-checkVariableNames Var name defs listaErrores =
-  | name in defs = 
-
-checkVariableNames Var 
-
-  para cada fundef
-    if variable (hoja):
-      if hoja not in fundef
-        append variable errores 
-    else
-      checkVariableNames defs expresiones
-
-# data Program = Program Defs Expr 
 
 checkProgram :: Program -> Checked
 checkProgram = do
