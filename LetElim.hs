855063f0074616db04c7f3c1caadc4d0c6372995
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-06-12 09:34:44 -0300

    [3] Let module

--- LetElim.hs
+++ LetElim.hs
@@ -17,7 +17,20 @@ import Data.List
 -- ELIMINACION DE LETs
 
 letElimP :: Program -> Program 
-letElimP  = undefined
+letElimP Program (Defs defs) (Expr x) = Program defs $ go x
+                                        where go ((Var|Integer|BoolLit) expr) = expr
+                                              go (Infix op expr1 expr2) = Infix (go op) (go expr1) (go expr2)
+                                              go (If condition then_expr else_expr) = (go condition) (go then_expr) (else_expr) 
+                                              go (Let (name, _) expr1 expr2) = subst name expr1 expr2
+                                              go (App name expr) = App name $ map go expr
 
 subst :: Name -> Expr -> Expr -> Expr 
-subst = undefined
+subst name expr1 (Name expr2) = if name == expr2 then expr2 else expr1
+subst name expr1 ((Integer|BoolLit) expr2) = expr2
+subst name expr1 (If condition then_expr else_expr) = 
+    If (subst name condition) (subst name then_expr) (subst name else_expr)
+subst name expr1 (Infix Op operand1 operand2) = 
+    Infix Op (subst name operand1) (subst name operand2)
+subst name expr1 (Let (TypedVar (name_let, type)) expr1_let expr2_let) =
+    subst name expr1 (subst name_let expr1_let expr2_let)
+subst name expr1 (App app_name expressions) = App app_name $ map (subst name) expressions
