e6b5594d507ed2e8eac3215d5a95ec0f38899c1f
Mathias Ramilo <mathiramilo2290@gmail.com>
2023-06-15 03:30:49 -0300

    Generator.hs implemented, it's necessary to run tests

--- Generator.hs
+++ Generator.hs
@@ -20,16 +20,9 @@ import Data.List (intercalate)
 genProgram :: Program -> String
 genProgram (Program defs expr) =
   let functionDecls = genFunctionDecls defs
-      mainFunctionLets = genMainExprLets expr
-      mainFunctionBdy = genMainExprBdy expr
-  in "#include <stdio.h>\n" ++ functionDecls ++ "\nint main() {\n" ++ mainFunctionLets ++ "printf(\"%d\\n\"," ++ mainFunctionBdy ++ "); }\n"
+      mainExpr = genMainExpr expr
+  in "#include <stdio.h>\n" ++ functionDecls ++ mainExpr
 
--- Funcion encargada de generar el codigo C de la expresion principal
-genMainExprBdy :: Expr -> String
-genMainExprBdy expr = undefined
-
-genMainExprLets :: Expr -> String
-genMainExprLets expr = undefined
 
 -- Funcion encargada de generar el codigo C de las declaraciones de funciones
 genFunctionDecls :: [FunDef] -> String
@@ -85,6 +78,45 @@ genFunctionDecl (FunDef (name, sig) params expr) =
         genArgs [] = ""
         genArgs [x] = fst(genExpr x)
         genArgs (x:xs) = fst(genExpr x) ++ "," ++ genArgs xs
+
+
+-- Funcion encargada de generar el codigo C de la expresion principal
+genMainExpr :: Expr -> String
+genMainExpr expr =
+  let (lets, body) = genExpr expr
+  in "\nint main() {\n" ++ lets ++ "printf(\"%d\\n\"," ++ body ++ "); }\n"
+  where
+    genExpr :: Expr -> (String, String)
+    genExpr (Var name) = ("_" ++ name, "")
+    genExpr (IntLit n) = (show n, "")
+    genExpr (BoolLit b) = (if b then "1" else "0", "")
+    genExpr (Infix op e1 e2) =
+      let opStr = genOp op
+          (arg1, _) = genExpr e1
+          (arg2, _) = genExpr e2
+      in ("(" ++ arg1 ++ opStr ++ arg2 ++ ")", "")
+    genExpr (If cond e1 e2) =
+      let (condition, _) = genExpr cond
+          (thenExpr, _) = genExpr e1
+          (elseExpr, _) = genExpr e2
+      in (condition ++ "?(" ++ thenExpr ++ "):(" ++ elseExpr ++ ")", "")
+    -- REVISAR: La transformacion de let a C no es de esta manera
+    genExpr (Let (name, typ) e1 e2) = 
+      let varType = genType typ
+          varName = "_let" ++ show randomInt
+          paramName = "_" ++ name
+          (expr1, _) = genExpr e1
+          (expr2, _) = genExpr e2
+      in (varName ++ "(" ++ expr1 ++ ")", varType ++ " " ++ varName ++ "(" ++ varType ++ paramName ++ "){\n" ++ "return (" ++ expr2 ++ "); };")
+    genExpr (App name args) =
+      let argList = genArgs args
+      in ("_" ++ name ++ "(" ++ argList ++ ")", "")
+      where
+        genArgs :: [Expr] -> String
+        genArgs [] = ""
+        genArgs [x] = fst(genExpr x)
+        genArgs (x:xs) = fst(genExpr x) ++ "," ++ genArgs xs
+
 
 genType :: Type -> String
 genType TyInt = "int"
